3c8078281274a4aa24f7bf389178fa24d29c5cee
qs33 <qs33@duke.edu>
2020-10-16 15:23:24 +0800

    HW59

--- 059_put_together/main.c
+++ 059_put_together/main.c
@@ -6,33 +6,47 @@
 #include "outname.h"
 
 counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
-  //WRITE ME
-  return NULL;
+  FILE * f = fopen(filename, "r");
+  if (f == NULL) {
+    fprintf(stderr, "Error opening file\n");
+    exit(EXIT_FAILURE);
+  }
+  counts_t * c = createCounts();
+  for (size_t i = 0; i < kvPairs->len; i++) {
+    addCount(c, kvPairs->kvarray[i]->value);
+  }
+  if (fclose(f) != 0) {
+    fprintf(stderr, "Error closing file\n");
+    exit(EXIT_FAILURE);
+  }
+  return c;
 }
 
 int main(int argc, char ** argv) {
-  //WRITE ME (plus add appropriate error checking!)
+ //WRITE ME (plus add appropriate error checking!)
  //read the key/value pairs from the file named by argv[1] (call the result kv)
-
+  kvarray_t * kv = readKVs(argv[1]);
  //count from 2 to argc (call the number you count i)
-
+  for (size_t i = 2; i < argc; i++) {
     //count the values that appear in the file named by argv[i], using kv as the key/value pair
     //   (call this result c)
-
+    counts_t * c = countFile(argv[i], kv);
     //compute the output file name from argv[i] (call this outName)
-
-
+    char * outName = computeOutputFileName(argv[i]);
     //open the file named by outName (call that f)
-
+    FILE * f = fopen(outName, "w");
     //print the counts from c into the FILE f
-
+    printCounts(c, f);
     //close f
-
+    if (fclose(f) != 0) {
+      fprintf(stderr, "Error closing file\n");
+      exit(EXIT_FAILURE);
+    }
     //free the memory for outName and c
-
-
-
+    free(outName);
+    freeCounts(c);
+  }
  //free the memory for kv
-
+  freeKVs(kv);
   return EXIT_SUCCESS;
 }
