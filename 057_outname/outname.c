34a058916ef0ea1671cec8f3413f19f45df0af9f
qs33 <qs33@duke.edu>
2020-10-16 10:55:17 +0800

    HW57

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -4,5 +4,17 @@
 #include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  size_t inputLen = strlen(inputName);
+  size_t nameLen = inputLen + 7;
+  char * temp = malloc((nameLen + 1) * sizeof(*temp));
+  strncpy(temp, inputName, strlen(inputName));
+  temp[inputLen] = '.';
+  temp[inputLen + 1] = 'c';
+  temp[inputLen + 2] = 'o';
+  temp[inputLen + 3] = 'u';
+  temp[inputLen + 4] = 'n';
+  temp[inputLen + 5] = 't';
+  temp[inputLen + 6] = 's';
+  temp[inputLen + 7] = '\0';
+  return temp;
 }
