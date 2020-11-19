ce2895cdd47364ff5f44df67f3abbd37737253cb
qs33 <qs33@duke.edu>
2020-11-19 22:23:44 +0800

    HW104

--- 104_huff_map/node.cpp
+++ 104_huff_map/node.cpp
@@ -1,5 +1,19 @@
 #include "node.h"
+#include <assert.h>
+
+void buildMap_helper(Node * current, BitString b, std::map<unsigned,BitString> & theMap) {
+  if (current->sym != NO_SYM) {
+    assert(current->left == NULL && current->right == NULL);
+    theMap[current->sym] = b;
+  }
+  else {
+    assert(current->left != NULL && current->right != NULL);
+    buildMap_helper(current->left, b.plusZero(), theMap);
+    buildMap_helper(current->right, b.plusOne(), theMap);
+  }
+}
+
 void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
-  //WRITE ME!
+  buildMap_helper(this, b, theMap);
 }
 
