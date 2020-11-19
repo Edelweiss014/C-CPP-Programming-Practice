b45cb683a370f991fe213d0ca71be7290f0b8ca2
qs33 <qs33@duke.edu>
2020-11-19 15:11:54 +0800

    HW103

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,18 @@
 #include "node.h"
 
 Node *  buildTree(uint64_t * counts) {
-  //WRITE ME!
+  priority_queue_t huff_queue;
+  for (unsigned i = 0; i < 257; i++) {
+    if (counts[i] == 0) continue;
+    huff_queue.push(new Node (i, counts[i]));
+  }
+  if (huff_queue.size() == 0) return NULL;
+  while (huff_queue.size() > 1) {
+    Node * temp1 = huff_queue.top();
+    huff_queue.pop();
+    Node * temp2 = huff_queue.top();
+    huff_queue.pop();
+    huff_queue.push(new Node (temp1, temp2));
+  }
+  return huff_queue.top();
 }  
