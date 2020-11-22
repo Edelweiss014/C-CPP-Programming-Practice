af6fbeffd12365769138e72001fd0d7f2f73425d
qs33 <qs33@duke.edu>
2020-11-22 19:51:24 +0800

    EVAL 3

--- 089_eval3/cyoa-step1.cpp
+++ 089_eval3/cyoa-step1.cpp
@@ -4,9 +4,9 @@
 #include "page.h"
 #include "cyoa.h"
 
-int main() {
-    std::string filename;
-    std::cin >> filename;
+int main(int argc, char ** argv) {
+    checkArgs(argc, 2);
+    std::string filename(argv[1]);
     Page p1(filename);
     p1.printPage();
     return EXIT_SUCCESS;
